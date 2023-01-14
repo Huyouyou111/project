22f4379fa6f9f76e0c45a3bc20cf5eb44cfc19c7
Huyouyou111 <122671837+Huyouyou111@users.noreply.github.com>
2023-01-14 18:21:30 +0800

    Add files via upload

---
+++ js/cban.js
@@ -0,0 +1,6 @@
+const menu_box=document.querySelector('.menu-box');
+const menu_button=document.querySelector('.menu-button');
+
+menu_button.addEventListener('click',function(){
+    menu_box.classList.toggle('active');
+})
